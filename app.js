9c5fc515549b98a1c418d87dfb4371afb3313e64
mrjannajafi <njfymrjan@gmail.com>
2024-11-10 03:07:08 -0800

    loop practice

--- app.js
+++ app.js
@@ -357,36 +357,80 @@
 // tall.
 // GOOD LUCK 😀
 
-const infoPerson1 = {
-  fristName : 'Mark' ,
-  lastName : 'Miller' , 
-  mass : 78 ,
-  height : 1.69 ,
-  calcBMI : function () {
-    this.bmi = this.mass / this.height ** 2 
-    return this.bmi
+// const infoPerson1 = {
+//   fristName : 'Mark' ,
+//   lastName : 'Miller' , 
+//   mass : 78 ,
+//   height : 1.69 ,
+//   calcBMI : function () {
+//     this.bmi = this.mass / this.height ** 2 
+//     return this.bmi
+//   }
+// }
+
+// const infoPerson2 = {
+//   fristName : 'John' ,
+//   lastName : 'Smith' , 
+//   mass : 200 ,
+//   height : 1.95 ,
+//   calcBMI : function () {
+//     this.bmi = this.mass / this.height ** 2 ;
+//     return this.bmi
+//   }
+// }
+
+// infoPerson1.calcBMI() ;
+// infoPerson2.calcBMI() ;
+
+
+// if (infoPerson1.bmi > infoPerson2.bmi) {
+//   console.log(`${infoPerson1.fristName}'s BMI ${infoPerson1.bmi} higher than ${infoPerson2.fristName}'s BMI ${infoPerson2.bmi}`);
+// }else if (infoPerson2.bmi > infoPerson1.bmi) {
+//   console.log(`${infoPerson2.fristName}'s BMI ${infoPerson2.bmi} higher than ${infoPerson1.fristName}'s BMI ${infoPerson1.bmi}`);
+// }else{
+//   console.log(`both are equall BMI`);
+// }
+
+// Let's improve Steven's tip calculator even more, this time using loops!
+// Your tasks:
+// 1. Create an array 'bills' containing all 10 test bill values
+// 2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+// 3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate
+// tips and total values (bill + tip) for every bill value in the bills array. Use a for
+// loop to perform the 10 calculations!
+// Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+// Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the
+// tips and totals arrays 😉
+// Bonus:
+// 4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as
+// an argument. This function calculates the average of all numbers in the given
+// array. This is a difficult challenge (we haven't done this before)! Here is how to
+// solve it:
+// 4.1. First, you will need to add up all values in the array. To do the addition,
+// start by creating a variable 'sum' that starts at 0. Then loop over the
+// array using a for loop. In each iteration, add the current value to the
+// 'sum' variable. This way, by the end of the loop, you have all values
+// added together
+// 4.2. To calculate the average, divide the sum you calculated before by the
+// length of the array (because that's the number of elements)
+// 4.3. Call the function with the 'totals' array
+// GOOD LUCK 😀
+
+const bills = [ 22, 295, 176, 440, 37, 105, 10, 1100, 86 , 52 ] ; 
+const totals = [] ;
+const tips = [] ;
+for (let i = 0; i < bills.length; i++) {
+  const calculates = bills[i] >= 50 && bills[i] <= 300 ? bills[i] * 0.15 : bills[i] * 0.2;
+  tips.push(calculates);
+  totals.push(calculates+bills[i]);
+}
+console.log(tips , totals);
+
+const calcAverage = function (arr) {
+  let sum = 0 ;
+  for (let i = 0; i < arr.length; i++) {
+sum += arr[i]    
   }
+  return sum / arr.length;
 }
-
-const infoPerson2 = {
-  fristName : 'John' ,
-  lastName : 'Smith' , 
-  mass : 200 ,
-  height : 1.95 ,
-  calcBMI : function () {
-    this.bmi = this.mass / this.height ** 2 ;
-    return this.bmi
-  }
-}
-
-infoPerson1.calcBMI() ;
-infoPerson2.calcBMI() ;
-
-
-if (infoPerson1.bmi > infoPerson2.bmi) {
-  console.log(`${infoPerson1.fristName}'s BMI ${infoPerson1.bmi} higher than ${infoPerson2.fristName}'s BMI ${infoPerson2.bmi}`);
-}else if (infoPerson2.bmi > infoPerson1.bmi) {
-  console.log(`${infoPerson2.fristName}'s BMI ${infoPerson2.bmi} higher than ${infoPerson1.fristName}'s BMI ${infoPerson1.bmi}`);
-}else{
-  console.log(`both are equall BMI`);
-}
+console.log(calcAverage(tips));
